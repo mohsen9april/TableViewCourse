2188e6eda87de7448e2d5b7351a614788c7cda26
Mohsen Abdollahi <mohsenabdollahi@mohsens-macbook-pro.local>
2019-05-12 17:11:23 +0430

    until to Section & Group

--- TableViewCourse/ViewController.swift
+++ TableViewCourse/ViewController.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource{
+    
+    
+    let myFruit :[[String]] = [["apple","watermelon","pineapple","melon","banana","orang","carrot"],
+                               ["Green","Yellow"    ,"Golddn"   ,"Grayg","GYellow","orange","Oragnyell"]
+                               ]
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return myFruit.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFruit[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        myCell.textLabel?.text = myFruit[indexPath.section][indexPath.row]
+        return myCell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
