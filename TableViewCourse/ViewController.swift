79a7ff3bb06f14e89f779c8d8b9d8f21939ea0ef
Mohsen Abdollahi <mohsenabdollahi@mohsens-macbook-pro.local>
2019-05-12 16:28:58 +0430

    Initial Commit

---
+++ TableViewCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewCourse
+//
+//  Created by Mohsen Abdollahi on 5/12/19.
+//  Copyright © 2019 Mohsen Abdollahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
